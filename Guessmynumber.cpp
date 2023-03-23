e5e4cfda39845e32e9538f6014f05980960a7bc7
unknown <santi.volcanos@gmail.com>
2023-03-23 09:50:08 -0600

    Función Guess

--- Guessmynumber.cpp
+++ Guessmynumber.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include<cstdlib> //Trabaja con números random
 using namespace std;
-
+int guessf();
+int guess;
 int main()
 {
 
@@ -9,9 +10,8 @@ int main()
     //srand(time(NULL));
     int randomNumber = rand();
     int secretNumber = (randomNumber % 100) + 1;
-    int guess=100;
     int tries = 0;
-    
+
 
     cout << "GUESS MY NUMBER" << endl;
     cout << "Adivina el número en el menor número de intentos posible." << endl;
@@ -20,26 +20,35 @@ int main()
     cout << secretNumber;
     do
     {
+
+
+        guessf();
+        tries++;
         int VC = secretNumber - guess;
-        if (VC >= -5 && VC <= 5)
+        if (VC >= -5 && VC <= 5 && VC != 0)
         {
             cout << "Estas muy cerca!\n";
         }
-        cout << "\nAdivina un numero! (1-100):" << endl;
-        cin >> guess;
-        tries++;
-      
         if (guess > secretNumber) {
             cout << "Muy Alto\n\n";
-          
+
         }
         else if (guess < secretNumber) {
             cout << "Muy Bajo\n\n";
 
         }
         else {
-            cout << "Excelente lo hiciste en -- " << tries << "-- intentos";
+            cout << "Excelente lo hiciste en --" << tries << "-- intentos";
 
         }
     } while (guess != secretNumber);
+}
+int guessf()
+{
+    do
+    {
+        cout << "\nAdivina un numero! (1-100):" << endl;
+        cin >> guess;
+    } while (guess > 100 || guess < 1);
+    return guess;
 }
